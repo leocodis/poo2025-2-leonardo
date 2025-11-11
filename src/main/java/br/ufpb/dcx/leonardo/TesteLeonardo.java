41e14699d225a59d5bb430d3428a2d5b768c85bc
alexandria <leonardo2018alexandria@gmail.com>
2025-11-11 09:19:50 -0300

    criando nova classe de teste usando IO

---
+++ src/main/java/br/ufpb/dcx/leonardo/TesteLeonardo.java
@@ -0,0 +1,10 @@
+package br.ufpb.dcx.leonardo;
+
+public class TesteLeonardo {
+    void main(){
+        IO.println("OI");
+        String nome = IO.readln("digite seu nome");
+        IO.println("oi "+ nome);
+    }
+
+}
